93f421ecc59c09ace5856c2c23c1fe53c29e53c4
LeaClover <plesen_14@mail.ru>
2022-07-25 03:38:52 +0300

    editting  hw task 62

--- HWtask62/Program.cs
+++ HWtask62/Program.cs
@@ -16,9 +16,24 @@ namespace GB.Task62
             FillMatrix(matrix);
             Console.WriteLine("Сгенерирован случайный массив размера 4х4: ");
             PrintMatrix(matrix);
-            GetSpiralSort(matrix);
             Console.WriteLine();
-            PrintMatrix(matrix);
+            int[] array = GetOneLineMatrix(matrix);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " | ");
+            }
+            Console.WriteLine();
+            int[] arraySort = GetArraySort(array);
+            Console.WriteLine();
+            for (int i = 0; i < arraySort.Length; i++)
+            {
+                Console.Write(arraySort[i] + " | ");
+            }
+            Console.WriteLine();
+            int[,] matrixNew = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            matrixNew = GetSpiralSort(matrix, arraySort);
+            Console.WriteLine();
+            PrintMatrix(matrixNew);
         }
 
         public static int[,] FillMatrix(int[,] matrixF)
@@ -39,16 +54,115 @@ namespace GB.Task62
             {
                 for (int j = 0; j < matrixP.GetLength(1); j++)
                 {
-                    Console.WriteLine(matrixP[i, j] + " | ");
+                    Console.Write(matrixP[i, j] + " | ");
                 }
                 Console.WriteLine();
             }
             return matrixP;
         }
 
-        public static int [,] GetSpiralSort(int [,] matrixSS)
+        public static int[] GetOneLineMatrix(int[,] matrix)
         {
-            
+            int[] array = new int[matrix.GetLength(0) * matrix.GetLength(1)];
+            int index = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    array[index] = matrix[i, j];
+                    index++;
+                }
+            }
+            return array;
+        }
+
+        public static int[] GetArraySort(int[] arrayS)
+        {
+            for (int i = 0; i < arrayS.Length; i++)
+            {
+                for (int j = 0; j < arrayS.Length - 1; j++)
+                {
+                    if (arrayS[j] > arrayS[j + 1])
+                    {
+                        int change = arrayS[j];
+                        arrayS[j] = arrayS[j + 1];
+                        arrayS[j + 1] = change;
+                    }
+                }
+
+            }
+            return arrayS;
+        }
+
+        public static int[,] GetTurn(int[,] matrixT)
+        {
+            int[,] matrixTurn = new int[matrixT.GetLength(0), matrixT.GetLength(1)];
+            for (int i = 0; i < matrixTurn.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixTurn.GetLength(1); j++)
+                {
+                    matrixTurn[j, i] = matrixT[i, matrixTurn.GetLength(1) - j - 1];
+                }
+            }
+            int[,] matrixS = new int[matrixTurn.GetLength(0), matrixTurn.GetLength(1)];
+            for (int i = 0; i < matrixS.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixS.GetLength(1); j++)
+                {
+                    matrixS[i, j] = matrixTurn[i, j];
+                }
+            }
+            return matrixS;
+        }
+
+        public static int[,] GetSpiralSort(int[,] matrixSS, int[] arraySS)
+        {
+            int[,] matrixSpiral = new int[matrixSS.GetLength(0), matrixSS.GetLength(1)];
+
+            for (int i = 0; i < matrixSpiral.GetLength(1); i++)
+            {
+                matrixSpiral[0, i] = arraySS[i];
+            }
+            matrixSpiral = GetTurn(matrixSpiral);
+
+            for (int i = 0; i < matrixSpiral.GetLength(1); i++)
+            {
+                matrixSpiral[0, i] = arraySS[i + matrixSpiral.GetLength(1) - 1];
+            }
+
+            matrixSpiral = GetTurn(matrixSpiral);
+
+            for (int i = 0; i < matrixSpiral.GetLength(1); i++)
+            {
+                matrixSpiral[0, i] = arraySS[i + (matrixSpiral.GetLength(1) - 1) * 2];
+            }
+            matrixSpiral = GetTurn(matrixSpiral);
+
+            for (int i = 0; i < matrixSpiral.GetLength(1)-1; i++)
+            {
+                matrixSpiral[0, i] = arraySS[i + (matrixSpiral.GetLength(1) - 1) * 3];
+            }
+            matrixSpiral = GetTurn(matrixSpiral);
+
+            for (int i = 1; i < matrixSpiral.GetLength(1)-1; i++)
+            {
+                matrixSpiral[1, i] = arraySS[i - 1 + (matrixSpiral.GetLength(1) - 1) * 4];
+            }
+            matrixSpiral = GetTurn(matrixSpiral);
+
+            for (int i = 2; i < matrixSpiral.GetLength(1)-1; i++)
+            {
+                matrixSpiral[1, i] = arraySS[i + (matrixSpiral.GetLength(1) - 1) * 4];
+            }
+
+            matrixSpiral = GetTurn(matrixSpiral);
+
+            for (int i = 2; i < matrixSpiral.GetLength(1)-1; i++)
+            {
+                matrixSpiral[1, i] = arraySS[i + 1 + (matrixSpiral.GetLength(1) - 1) * 4];
+            }
+            matrixSpiral = GetTurn(GetTurn(matrixSpiral));
+            return matrixSpiral;
         }
     }
 }
